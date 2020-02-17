861776746d83cef07836cf65acbd040cbf1a8fa6
ananya92 <1992ananya@gmail.com>
2020-02-18 00:33:33 +1030

    Added functions to access database in orm.js

---
+++ config/orm.js
@@ -0,0 +1,34 @@
+var connection = require("./connection.js");
+
+var orm = {
+  selectAll: function(tableName, cb) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, [tableName], function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  insertOne: function(tableName, colVal, cb) {
+    var queryString = "INSERT INTO ??(burger_name) VALUES (??)";
+    console.log(queryString);
+    connection.query(queryString, [tableName, colVal], function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  UpdateOne: function(tableName, setColName, setColVal, whrColName, whrColVal, cb) {
+    var queryString =
+      "UPDATE TABLE ?? SET ?? = ?? WHERE ?? = ??";
+
+    connection.query(
+      queryString,
+      [tableName, setColName, setColVal, whrColName, whrColVal],
+      function(err, result) {
+        if (err) throw err;
+        cb(result);
+      }
+    );
+  }
+};
+
+module.exports = orm;
