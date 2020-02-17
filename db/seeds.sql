933d9f8fedb27fb096f124950955bfb31b532d6b
ananya92 <1992ananya@gmail.com>
2020-02-17 23:31:16 +1030

    Adding initial folder structure and files. Created the database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+insert into burgers(burger_name) values ("Chicken mayo burger");
+insert into burgers(burger_name) values ("Egg cheese burger");
+insert into burgers(burger_name) values ("Veggie burger");
+
