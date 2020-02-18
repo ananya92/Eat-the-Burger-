15b596d3469cf8b700c898e2aa334bfce1753760
ananya92 <1992ananya@gmail.com>
2020-02-18 16:32:05 +1030

    Fixed the query definition in ORM

--- config/orm.js
+++ config/orm.js
@@ -2,28 +2,23 @@ var connection = require("./connection.js");
 
 var orm = {
   selectAll: function(tableName, cb) {
-    var queryString = "SELECT * FROM ??";
-    connection.query(queryString, [tableName], function(err, result) {
+    var queryString = "SELECT * FROM " + tableName;
+    connection.query(queryString, function(err, result) {
       if (err) throw err;
       cb(result);
     });
   },
   insertOne: function(tableName, colName, colVal, cb) {
-    var queryString = "INSERT INTO ??(??) VALUES (??)";
+    var queryString = `INSERT INTO  ${tableName} (${colName}) VALUES ("${colVal}")`;
     console.log(queryString);
-    connection.query(queryString, [tableName, colName, colVal], function(err, result) {
+    connection.query(queryString, function(err, result) {
       if (err) throw err;
       cb(result);
     });
   },
   updateOne: function(tableName, setColName, setColVal, whrColName, whrColVal, cb) {
-    var queryString =
-      "UPDATE TABLE ?? SET ?? = ?? WHERE ?? = ??";
-
-    connection.query(
-      queryString,
-      [tableName, setColName, setColVal, whrColName, whrColVal],
-      function(err, result) {
+    var queryString = "UPDATE " + tableName + " SET " + setColName + " = " + setColVal + " WHERE " + whrColName + " = " + whrColVal;
+    connection.query(queryString, function(err, result) {
         if (err) throw err;
         cb(result);
       }
